4f2e2586b4a06dd02145d6d60e5b5f43906a2861
Sam Perry <s.sperry94@gmail.com>
2024-02-06 13:21:05 -0600

    Added the main body of the code
    added everything under the main function, only consists of user indicating
    wheather they are a teacher or a student.

--- main.cpp
+++ main.cpp
@@ -3,24 +3,60 @@
 
 //maybe make a python script to verify user info? 
 //should see about maybe making a GUI at some point
-//should put on Git soon
 //write real documentation on how to use, functions, etc
 
 //CURRENTLY: redoing main.cpp
 
-void print_vector(std::vector <std::string> vector)
-{
-    std::cout << "{";
-    for(std::string item : vector)
-    {
-        std::cout << item << ", ";
-    }
-    std::cout << "}\n";
-}
+//code with teacher options
+void teacher();
+
+//code with student options
+void student();
 
 int main()
 {
+    char input;
 
+    std::cout << "Welcome to the Student Manager Program!\n";
+    while(input != 'q')
+    {
+        std::cout << "Are you a student or a teacher (S/T)?\n";
+        std::cin >> input;
 
+        if(input == 'S' || input == 's')
+        {
+            student();
+            
+            //delete me
+            std::cout << "Running student options...\n";
+            break;
+        }
+        else if(input == 'T' || input == 't')
+        {
+            teacher();
+            //delete me
+            std::cout << "Running teacher options...\n";
+            break;
+        }
+        else if(input == 'Q' || input == 'q')
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Please enter either a 'S' for student, 'T' for teacher, or 'q' to quit. Your response is not caps sensitive.\n";
+        }
+    }
+    std::cout << "Goodbye!\n";
     return 0;
+}
+
+void teacher()
+{
+    //some code
+}
+
+void student()
+{
+    //some code
 }
