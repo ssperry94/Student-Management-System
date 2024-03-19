330d7aca59deefeaf3e62920bd1d2e73d0a9cdab
Sam Perry <s.sperry94@gmail.com>
2024-03-19 15:46:30 -0500

    BRANCH:UserAuth.....created base python source file with imports only.

---
+++ authenticate.py
@@ -0,0 +1,10 @@
+# User Authentication script
+
+#need to use fernet
+#need to recieve data from main.cpp, validate input data against known userbase, then send back either true or false (maybe 1 or 0)
+#need to get fernet stuff from my desktop onto the cloud first
+
+#imports
+import sys
+from cryptography.fernet import Fernet
+
